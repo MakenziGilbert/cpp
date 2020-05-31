81cfbc8ee839a203956d3dbef7f42d2330389964
Maksym Skorbatiuk <57547446+MakenziGilbert@users.noreply.github.com>
2020-05-31 19:46:04 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,69 +7,108 @@ using namespace std;
 bool work = 1, working = 1;
 char change = ' ';
 int code;
-double a, b, c, x, y, x1, x2;;
-int k; 
-float scale = 1, step = 1;
-int otstup = 12;
+float step = 1;
+double a, b, c, d, x, y, x1, x2;;
+int otstup = 120;
+int w;
+int h;
 
 void Oxy(HDC, int, int);
 void control(char);
 void menu();
+bool debug_cin();
 
 int main()
 {
-	setlocale(LC_ALL, "Russian");
+	setlocale(LC_ALL, "");
 	HDC hdc = GetDC(GetConsoleWindow());
 	HPEN Pen = CreatePen(PS_SOLID, 2, RGB(255, 255, 255));
 	SelectObject(hdc, Pen);
 	int fun;
 	int Ox, Oy;
+	HWND hWnd = GetConsoleWindow();
+	RECT rc;
+	GetClientRect(hWnd, &rc);
+	w = rc.right; // ширина рабочей области
+	h = rc.bottom;// высота рабочей области
+	h /= 20;
+	h *= 20;
+	w /= 20;
+	w *= 20;
 	
 	while (working)
 	{
-		cout << "Выберите какой график вы хотите увидеть:\n1) у = ах2 + bх + с\n2) у = a * sin(x) + cos(b)\n>";
-		cin >> fun;
-		system("cls");
-		cout << "Введите размер поля по координате Оx: ";
-		cin >> Ox;
-		cout << "Введите размер поля по координате Оy: ";
-		cin >> Oy;
-		system("cls");
+		do
+		{
+			cout << "Виберiть який графiк ви хочете побачити:\n\
+1) у = ах2 + bх + с\n\
+2) у = a * sin(x) + cos(b)\n\
+3) y = a + (b / c * x + d)\n\
+4) y = ax3 + bx2 + cx + d\n\
+5) y = (x^a + b) / (x^c - d)\n\
+6) y = ax^b + cx^d\n\
+7) y = a * b^x + c\n\
+8) y = a * e^(b * x)\n\
+9) Дельтоiд (Крива Штейнера)\n\
+10) Астроiда\n>";
+			cin >> fun;
+			system("cls");
 
-		cout << "Введите размер отрезка\nот х - ";
-		cin >> x1;
-		cout << "до х - ";
-		cin >> x2;
-		system("cls");
-		work = 1;
+		} while (debug_cin());
+
+		if (fun <= 10 && fun > 0)
+		{
+			do
+			{
+				do
+				{
+					cout << "Введiть розмiр поля по координатi Оx: ";
+					cin >> Ox;
+					cout << "Введiть розмiр поля по координатi Оy: ";
+					cin >> Oy;
+					system("cls");
+				} while (Ox < 0 && Oy < 0);
+
+				cout << "Введiть розмiр вiдрiзка\nвiд х1 - ";
+				cin >> x1;
+				cout << "до х2 - ";
+				cin >> x2;
+				system("cls");
+				work = 1;
+			} while (debug_cin());
+		}
 
 		switch (fun)
 		{
 		case 1:// у = ах2 + bх + с
 		{
-			cout << "Введите значения а: ";
-			cin >> a;
-			cout << "Введите значения b: ";
-			cin >> b;
-			cout << "Введите значения c: ";
-			cin >> c;
+			do
+			{
+				cout << "Введiть значення а: ";
+				cin >> a;
+				cout << "Введiть значення b: ";
+				cin >> b;
+				cout << "Введiть значення c: ";
+				cin >> c;
+
+				system("cls");
+			} while (debug_cin());
 
 			while (work)
 			{
 				system("cls");
-
 				menu();
 
-				cout << "График функции: y = " << a << "x^2 + " << b << "x + " << c << "  От х - " << x1 << " до х - " << x2;
+				cout << "Графiк функцiї: y = " << a << "x^2 + " << b << "x + " << c << "  От х - " << x1 << " до х - " << x2;
 
 				Oxy(hdc, Ox, Oy);
 
 				for (double xi = x1; xi < x2; xi += 0.01f)
 				{
-					if ((a * pow(xi, 2) + b * xi + c) < Oy && (a * pow(xi, 2) + b * xi + c)  > -Oy)
+					if ((-((h - otstup) / (Oy * 2)) * (a * pow(xi, 2) + b * xi + c) + (h - otstup) / 2) + otstup < h && -((h - otstup) / (Oy * 2)) * (a * pow(xi, 2) + b * xi + c) + (h - otstup) / 2 > 0)
 					{
-						MoveToEx(hdc, (10 * xi + Ox * 10) * scale,		(-10 * (a * pow(xi, 2) + b * xi + c) + Oy * 10  + otstup * 10) * scale, NULL);
-						LineTo(hdc,   (10 * xi + Ox * 10) * scale,		(-10 * (a * pow(xi, 2) + b * xi + c) + Oy * 10  + otstup * 10) * scale);
+						MoveToEx(hdc, w / 2 + xi * (w / (Ox * 2)), (-((h - otstup) / (Oy * 2)) * (a * pow(xi, 2) + b * xi + c) + (h - otstup) / 2) + otstup, NULL);
+						LineTo(hdc, w / 2 + xi * (w / (Ox * 2)), (-((h - otstup) / (Oy * 2)) * (a * pow(xi, 2) + b * xi + c) + (h - otstup) / 2) + otstup);
 					}
 				}
 				control(_getch());
@@ -78,35 +117,337 @@ int main()
 		}
 		case 2: // у = a * sin(x) + cos(b)
 		{
-			cout << "Введите значения а: ";
-			cin >> a;
-			cout << "Введите значения b: ";
-			cin >> b;
+			do
+			{
+				cout << "Введiть значення а: ";
+				cin >> a;
+				cout << "Введiть значення b: ";
+				cin >> b;
+
+				system("cls");
+			} while (debug_cin());
 
 			while (work)
 			{
 				system("cls");
-
 				menu();
 
-				cout << "График функции: y = " << a << " * sin(x) + cos(" << b << "). От х - " << x1 << " до х - " << x2;
+				cout << "Графiк функцiї: y = " << a << " * sin(x) + cos(" << b << "). Вiд х - " << x1 << " до х - " << x2;
 
 				Oxy(hdc, Ox, Oy);
 
 				for (double xi = x1; xi < x2; xi += 0.01f)
 				{
-					if ((a * sin(xi) + cos(b)) < Oy && (a * sin(xi) + cos(b)) > -Oy)
+					if ((-((h - otstup) / (Oy * 2)) * (a * sin(xi) + cos(b)) + (h - otstup) / 2) + otstup < h && (-((h - otstup) / (Oy * 2)) * (a * sin(xi) + cos(b)) + (h - otstup) / 2) > 0)
 					{
-						MoveToEx(hdc,	(10 * xi + Ox * 10) * scale,	(-10 * (a * sin(xi) + cos(b)) + Oy * 10 + otstup * 10) * scale, NULL);
-						LineTo(hdc,		(10 * xi + Ox * 10) * scale,	(-10 * (a * sin(xi) + cos(b)) + Oy * 10 + otstup * 10) * scale);
+						MoveToEx(hdc, w / 2 + xi * (w / (Ox * 2)), (-((h - otstup) / (Oy * 2)) * (a * sin(xi) + cos(b)) + (h - otstup) / 2) + otstup, NULL);
+						LineTo(hdc, w / 2 + xi * (w / (Ox * 2)), (-((h - otstup) / (Oy * 2)) * (a * sin(xi) + cos(b)) + (h - otstup) / 2) + otstup);
 					}
 				}
 				control(_getch());
 			}
 			break;
 		}
-		default:
-			cout << "Ошибочный ввод" << endl;
+		case 3: // a + (b / (c * x + d)); 
+		{
+			do
+			{
+				cout << "Введiть значення а: ";
+				cin >> a;
+				cout << "Введiть значення b: ";
+				cin >> b;
+				cout << "Введiть значення c: ";
+				cin >> c;
+				cout << "Введiть значення d: ";
+				cin >> d;
+
+				system("cls");
+			} while (debug_cin());
+
+			while (work)
+			{
+				system("cls");
+				menu();
+
+				cout << "Графiк функцiї: y = " << a << " + (" << b << " / " << c << "x + " << d << "). Вiд х - " << x1 << " до х - " << x2;
+
+				Oxy(hdc, Ox, Oy);
+
+				for (double xi = x1; xi < x2; xi += 0.01f)
+				{
+					if ((-((h - otstup) / (Oy * 2)) * (a + (b / (c * xi + d))) + (h - otstup) / 2) + otstup < h && (-((h - otstup) / (Oy * 2)) * (a + (b / (c * xi + d))) + (h - otstup) / 2) > 0 && c * xi + d != 0)
+					{
+						MoveToEx(hdc, w / 2 + xi * (w / (Ox * 2)), (-((h - otstup) / (Oy * 2)) * (a + (b / (c * xi + d))) + (h - otstup) / 2) + otstup, NULL);
+						LineTo(hdc, w / 2 + xi * (w / (Ox * 2)), (-((h - otstup) / (Oy * 2)) * (a + (b / (c * xi + d))) + (h - otstup) / 2) + otstup);
+					}
+				}
+				control(_getch());
+			}
+			break;
+		}
+		case 4: // ax3 + bx2 + cx + d; 
+		{
+			do
+			{
+				cout << "Введiть значення а: ";
+				cin >> a;
+				cout << "Введiть значення b: ";
+				cin >> b;
+				cout << "Введiть значення c: ";
+				cin >> c;
+				cout << "Введiть значення d: ";
+				cin >> d;
+
+				system("cls");
+			} while (debug_cin());
+
+			while (work)
+			{
+				system("cls");
+				menu();
+
+				cout << "Графiк функцiї: y = " << a << "x^3 + " << b << "x^2 + " << c << "x + " << d << ". Вiд х - " << x1 << " до х - " << x2;
+
+				Oxy(hdc, Ox, Oy);
+
+				for (double xi = x1; xi < x2; xi += 0.01f)
+				{
+					if ((-((h - otstup) / (Oy * 2)) * (a * pow(xi, 3) + b * pow(xi, 2) + c * xi + d) + (h - otstup) / 2) + otstup < h && -((h - otstup) / (Oy * 2)) * (a * pow(xi, 3) + b * pow(xi, 2) + c * xi + d) + (h - otstup) / 2 > 0)
+					{
+						MoveToEx(hdc, w / 2 + xi * (w / (Ox * 2)), (-((h - otstup) / (Oy * 2)) * (a * pow(xi, 3) + b * pow(xi, 2) + c * xi + d) + (h - otstup) / 2) + otstup, NULL);
+						LineTo(hdc, w / 2 + xi * (w / (Ox * 2)), (-((h - otstup) / (Oy * 2)) * (a * pow(xi, 3) + b * pow(xi, 2) + c * xi + d) + (h - otstup) / 2) + otstup);
+					}
+				}
+				control(_getch());
+			}
+			break;
+		}
+		case 5: // y = (x^a + b) / (x^c - d)
+		{
+			do
+			{
+				cout << "Введiть значення а: ";
+				cin >> a;
+				cout << "Введiть значення b: ";
+				cin >> b;
+				cout << "Введiть значення c: ";
+				cin >> c;
+				cout << "Введiть значення d: ";
+				cin >> d;
+
+				system("cls");
+			} while (debug_cin());
+
+			while (work)
+			{
+				system("cls");
+				menu();
+
+				cout << "Графiк функцiї: y = (x^" << a << " + " << b << ") / (x^" << c << " - " << d << "). Вiд х - " << x1 << " до х - " << x2;
+
+				Oxy(hdc, Ox, Oy);
+
+				for (double xi = x1; xi < x2; xi += 0.01f)
+				{
+					if ((-((h - otstup) / (Oy * 2)) * ((pow(xi, a) + b) / (pow(xi, c) - d)) + (h - otstup) / 2) + otstup < h && -((h - otstup) / (Oy * 2)) * ((pow(xi, a) + b) / (pow(xi, c) - d)) + (h - otstup) / 2 > 0 && (pow(xi, c) - d) != 0)
+					{
+						MoveToEx(hdc, w / 2 + xi * (w / (Ox * 2)), (-((h - otstup) / (Oy * 2)) * ((pow(xi, a) + b) / (pow(xi, c) - d)) + (h - otstup) / 2) + otstup, NULL);
+						LineTo(hdc, w / 2 + xi * (w / (Ox * 2)), (-((h - otstup) / (Oy * 2)) * ((pow(xi, a) + b) / (pow(xi, c) - d)) + (h - otstup) / 2) + otstup);
+					}
+				}
+				control(_getch());
+			}
+			break;
+		}
+		case 6: // y = ax^b + cx^d
+		{
+			do
+			{
+				cout << "Введiть значення а: ";
+				cin >> a;
+				cout << "Введiть значення b: ";
+				cin >> b;
+				cout << "Введiть значення c: ";
+				cin >> c;
+				cout << "Введiть значення d: ";
+				cin >> d;
+
+				system("cls");
+			} while (debug_cin());
+
+			while (work)
+			{
+				system("cls");
+				menu();
+
+				cout << "Графiк функцiї: y = " << a << "x^" << b << " + " << c << "x^" << d << ". Вiд х - " << x1 << " до х - " << x2;
+
+				Oxy(hdc, Ox, Oy);
+
+				for (double xi = x1; xi < x2; xi += 0.01f)
+				{
+					if ((-((h - otstup) / (Oy * 2)) * (a * pow(xi, b) + c * pow(xi, d)) + (h - otstup) / 2) + otstup < h && (-((h - otstup) / (Oy * 2)) * (a * pow(xi, b) + c * pow(xi, d)) + (h - otstup) / 2) > 0)
+					{
+						MoveToEx(hdc, w / 2 + xi * (w / (Ox * 2)), (-((h - otstup) / (Oy * 2)) * (a * pow(xi, b) + c * pow(xi, d)) + (h - otstup) / 2) + otstup, NULL);
+						LineTo(hdc, w / 2 + xi * (w / (Ox * 2)), (-((h - otstup) / (Oy * 2)) * (a * pow(xi, b) + c * pow(xi, d)) + (h - otstup) / 2) + otstup);
+					}
+				}
+				control(_getch());
+			}
+			break;
+		}
+		case 7: // y = a * b^x + c
+		{
+			do
+			{
+				cout << "Введiть значення а: ";
+				cin >> a;
+				cout << "Введiть значення b: ";
+				cin >> b;
+				cout << "Введiть значення c: ";
+				cin >> c;
+
+				system("cls");
+			} while (debug_cin());
+
+			while (work)
+			{
+				system("cls");
+				menu();
+
+				cout << "Графiк функцiї: y = " << a << " * " << b << "^x + " << c << ". Вiд х - " << x1 << " до х - " << x2;
+
+				Oxy(hdc, Ox, Oy);
+
+				for (double xi = x1; xi < x2; xi += 0.01f)
+				{
+					if ((-((h - otstup) / (Oy * 2)) * (a * pow(b, xi) + c) + (h - otstup) / 2) + otstup < h && (-((h - otstup) / (Oy * 2)) * (a * pow(b, xi) + c) + (h - otstup) / 2) > 0)
+					{
+						MoveToEx(hdc, w / 2 + xi * (w / (Ox * 2)), (-((h - otstup) / (Oy * 2)) * (a * pow(b, xi) + c) + (h - otstup) / 2) + otstup, NULL);
+						LineTo(hdc, w / 2 + xi * (w / (Ox * 2)), (-((h - otstup) / (Oy * 2)) * (a * pow(b, xi) + c) + (h - otstup) / 2) + otstup);
+					}
+				}
+				control(_getch());
+			}
+			break;
+		}
+		case 8: // y = a * e^(b * x)
+		{
+			do
+			{
+				cout << "Введiть значення а: ";
+				cin >> a;
+				cout << "Введiть значення b: ";
+				cin >> b;
+
+				system("cls");
+			} while (debug_cin());
+
+			while (work)
+			{
+				system("cls");
+				menu();
+
+				cout << "Графiк функцiї: y = " << a << " * e^(" << b << " * x). Вiд х - " << x1 << " до х - " << x2;
+
+				Oxy(hdc, Ox, Oy);
+
+				for (double xi = x1; xi < x2; xi += 0.01f)
+				{
+					if ((-((h - otstup) / (Oy * 2)) * (a * exp(b * xi)) + (h - otstup) / 2) + otstup < h && (-((h - otstup) / (Oy * 2)) * (a * exp(b * xi)) + (h - otstup) / 2) > 0)
+					{
+						MoveToEx(hdc, w / 2 + xi * (w / (Ox * 2)), (-((h - otstup) / (Oy * 2)) * (a * exp(b * xi)) + (h - otstup) / 2) + otstup, NULL);
+						LineTo(hdc, w / 2 + xi * (w / (Ox * 2)), (-((h - otstup) / (Oy * 2)) * (a * exp(b * xi)) + (h - otstup) / 2) + otstup);
+					}
+				}
+				control(_getch());
+			}
+			break;
+		}
+		case 9: // x = acos(t) + cos(bt), y = csin(t) − sin(dt)
+		{
+			do
+			{
+				cout << "Рекомендованно вводити одноковi значення для всiх параметрiв" << endl;
+				cout << "Введiть значення а: ";
+				cin >> a;
+				cout << "Введiть значення b: ";
+				cin >> b;
+				cout << "Введiть значення c: ";
+				cin >> c;
+				cout << "Введiть значення d: ";
+				cin >> d;
+
+				system("cls");
+			} while (debug_cin());
+
+			x1 = -Ox;
+			x2 = Ox;
+
+			while (work)
+			{
+				system("cls");
+				menu();
+
+				cout << "Графiк функцiї: x = " << a << " cos(t) + cos(" << b <<"t) , y = "<< c <<" sin(t) - sin("<< d <<"t). Вiд t - " << x1 << " до t - " << x2;
+
+				Oxy(hdc, Ox, Oy);
+
+				for (double xi = x1; xi < x2; xi += 0.01f)
+				{
+					if ((-((h - otstup) / (Oy * 2)) * (c * sin(xi) - sin(d * xi)) + (h - otstup) / 2) + otstup < h && (-((h - otstup) / (Oy * 2)) * (c * sin(xi) - sin(d * xi)) + (h - otstup) / 2) > 0)
+						if (w / 2 + (a * cos(xi) + cos(b * xi)) * (w / (Ox * 2)) < w && w / 2 + (a * cos(xi) + cos(b * xi)) * (w / (Ox * 2)) > 0)
+						{
+							MoveToEx(hdc, w / 2 + (a * cos(xi) + cos(b * xi)) * (w / (Ox * 2)), (-((h - otstup) / (Oy * 2)) * (c * sin(xi) - sin(d * xi)) + (h - otstup) / 2) + otstup, NULL);
+							LineTo(hdc, w / 2 + (a * cos(xi) + cos(b * xi)) * (w / (Ox * 2)), (-((h - otstup) / (Oy * 2)) * (c * sin(xi) - sin(d * xi)) + (h - otstup) / 2) + otstup);
+						}
+				}
+				control(_getch());
+			}
+			break;
+		}
+		case 10: // x = 2(sin(t))^3, y = 2(cos(t))^3
+		{
+			do
+			{
+				cout << "Рекомендованно значення а - 2, b - 3, c - 2, d - 3" << endl;
+				cout << "Введiть значення а: ";
+				cin >> a;
+				cout << "Введiть значення b: ";
+				cin >> b;
+				cout << "Введiть значення c: ";
+				cin >> c;
+				cout << "Введiть значення d: ";
+				cin >> d;
+
+				system("cls");
+			} while (debug_cin());
+
+			x1 = -Ox;
+			x2 = Ox;
+
+			while (work)
+			{
+				system("cls");
+				menu();
+
+				cout << "Графiк функцiї: x = " << a << " (sin(t))^" << b << ", y = " << c << " (cos(t))^" << d << ". Вiд t - " << x1 << " до t - " << x2;
+
+				Oxy(hdc, Ox, Oy);
+
+				for (double xi = x1; xi < x2; xi += 0.01f)
+				{
+					if ((-((h - otstup) / (Oy * 2)) * (c * pow(cos(xi), d)) + (h - otstup) / 2) + otstup < h && (-((h - otstup) / (Oy * 2)) * (c * pow(cos(xi), d)) + (h - otstup) / 2) > 0)
+						if (w / 2 + (a * pow(sin(xi), b)) * (w / (Ox * 2)) < w && w / 2 + (a * pow(sin(xi), b)) * (w / (Ox * 2)) > 0)
+						{
+							MoveToEx(hdc, w / 2 +	(a * pow(sin(xi), b)) * (w / (Ox * 2)),		(-((h - otstup) / (Oy * 2)) * (c * pow(cos(xi), d)) + (h - otstup) / 2) + otstup, NULL);
+							LineTo(hdc, w / 2 +		(a * pow(sin(xi), b)) * (w / (Ox * 2)),		(-((h - otstup) / (Oy * 2)) * (c * pow(cos(xi), d)) + (h - otstup) / 2) + otstup);
+						}
+				}
+				control(_getch());
+			}
+			break;
+		}
 		}
 	}
 	return 0;
@@ -114,29 +455,28 @@ int main()
 
 void Oxy(HDC hdc, int Ox, int Oy)
 {
-	MoveToEx(hdc,	0,						(Oy * 10 + otstup * 10) * scale, NULL);//Ox
-	LineTo(hdc,		(Ox * 20) * scale,		(Oy * 10 + otstup * 10) * scale);
+	MoveToEx(hdc, 0, h / 2 + otstup / 2, NULL);//Ox
+	LineTo(hdc, w, h / 2 + otstup / 2);
 
-	MoveToEx(hdc,	(Ox * 10) * scale,		(otstup * 10) * scale, NULL);//Oy
-	LineTo(hdc,		(Ox * 10) * scale,		(Oy * 20 + otstup * 10) * scale);
+	MoveToEx(hdc, w / 2, otstup, NULL);//Oy
+	LineTo(hdc, w / 2, h);
 
-	for (int i = 0; i <= Ox * 2; i += 1)
+	for (int i = 0; i <= w; i += w / (Ox * 2))
 	{
-		MoveToEx(hdc,	(i * 10) * scale,	(Oy * 10 + otstup * 10) * scale, NULL);
-		LineTo(hdc,		(i * 10) * scale,	(Oy * 10 - 5 + otstup * 10) * scale);
+		MoveToEx(hdc, i, h / 2 + otstup / 2, NULL);
+		LineTo(hdc, i, (h / 2 - 10) + otstup / 2);
 	}
 
-	for (float j = otstup * scale; j <= (Oy * 2 + otstup) * scale; j += scale)
+	for (float j = otstup; j <= h; j += (h - otstup) / (Oy * 2))
 	{
-		MoveToEx(hdc,	(Ox * 10) * scale,		j * 10, NULL);
-		LineTo(hdc,		(Ox * 10 + 5) * scale,  j * 10);
-	} 
+		MoveToEx(hdc, w / 2, j, NULL);
+		LineTo(hdc, (w / 2 + 10), j);
+	}
 }
 
 void control(char ch)
 {
 	code = static_cast<int>(ch);
-	cout << code;
 
 	switch (code)
 	{
@@ -161,12 +501,12 @@ void control(char ch)
 	case 67:
 		change = 'c';
 		break;
-	case 120:
-	case 88:
-		change = 'x';
+	case 100:
+	case 68:
+		change = 'd';
 		break;
 	case 46:
-		change = '> b';
+		change = '>';
 		break;
 	case 44:
 		change = '<';
@@ -176,6 +516,9 @@ void control(char ch)
 		break;
 	case 109:
 		step = 1;
+		break;
+	case 79:
+		step = 10;
 		break;
 
 	case 77:
@@ -190,8 +533,8 @@ void control(char ch)
 		case 'c':
 			c += step;
 			break;
-		case 'x':
-			scale += step;
+		case 'd':
+			d += step;
 			break;
 		case '>':
 			x2 += step;
@@ -214,8 +557,8 @@ void control(char ch)
 		case 'c':
 			c -= step;
 			break;
-		case 'x':
-			scale -= step;
+		case 'd':
+			d -= step;
 			break;
 		case '>':
 			x2 -= step;
@@ -230,9 +573,24 @@ void control(char ch)
 
 void menu()
 {
-	cout << "Что-бы изенить одну из констант выберите её на клавиатуре." << endl;
-	cout << "Для увеличения/уменимьшения маштаба нажмите \"х\". Маштаб - x" << scale << endl;
-	cout << "Для изменения начала отрезка нажмите \"<\" а для конца отрезка \">\"" << endl;
-	cout << "Сейчас выбрана \"" << change << "\". Шаг - " << step << " ( стрелками в право +1, в лево - 1, N - шаг 0.1, M - шаг 1)" << endl;
-	cout << "Что-бы вернуться в меню нажмине стрелку вверх, что-бы закрыть приложение стрелку вниз." << endl;
+	cout << "Щоб змiнити одну з констант виберiть її на клавiатурi." << endl;
+	cout << "Для змiни початку вiдрiзка натиснiть \"<\" а для кiнця вiдрiзка \">\"" << endl;
+	cout << "Зараз обрана \"" << change << "\". Крок - " << step << " (стрiлками вправо +1, влiво - 1, N - крок 0.1, M - крок 1)" << endl;
+	cout << "Що-б повернутися в меню натиснiть стрiлку вгору, щоб закрити програму стрiлку вниз." << endl;
+	cout << "Для того щоб обновити графiк натисніт пробiл" << endl;
+}
+
+bool debug_cin()
+{
+	if (cin.fail())
+	{
+		cin.clear();
+		cin.ignore(200000, '\n');
+		cout << "Не коректне введення! Спробуйте ще раз.\n\n";
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
